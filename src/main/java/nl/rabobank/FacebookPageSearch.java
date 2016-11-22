892f6107e932f523d2246269ba78b06113074e70
jayaramj <Jagannathan.Jayaraman@rabobank.nl>
2016-11-22 16:52:52 +0100

    getting all comments from the post

--- src/main/java/nl/rabobank/FacebookPageSearch.java
+++ src/main/java/nl/rabobank/FacebookPageSearch.java
@@ -23,9 +23,9 @@ import com.restfb.types.Post;
  */
 public class FacebookPageSearch {
 
-    private static final String[] SEARCH_TERM = { "rabo", "abn amro", "ING bank" };
-    
- // private static final String[] SEARCH_TERM =
+    private static final String[] SEARCH_TERM = { "rabobank", "abn amro", "ING Nederland" };
+
+    // private static final String[] SEARCH_TERM =
     // {"Kaart","pinpas","bankpas","Mobiel","mobiel bankieren","internet bankieren","internet banking","sparen","spaarrekening"};
 
 
@@ -48,64 +48,27 @@ public class FacebookPageSearch {
         /** Search Page **/
         Connection<Page> pageResults = fbClient.fetchConnection("search", Page.class, Parameter.with("q", searchText),
                 Parameter.with("type", "page"));
-
-        int noOfPagesSearched = 0;
-        for (List<Page> pages : pageResults) {
-            for (Page page : pages) {
-
-                System.out.println("fb.com/" + page.getId());
-
-                int noOfPosts = 0;
-                Connection<Post> postFeed = fbClient.fetchConnection(page.getId() + "/feed", Post.class);
-    //            System.out.println("DAta from post feeds=====" + postFeed.getData());
-//                for (List<Post> posts : postFeed) {
-//                    System.out.println(postFeed.getData());
-//                    if (noOfPosts > 15) {
-//                        break;
-//                    }
-                    int noOfPost = 0;
-                    for (Post post : postFeed.getData()) {
-                        if (noOfPost > 15) {
-                            break;
-                        }
-                        // TODO : Check post.getName() gives correct person name, check if we can get email id of the
-                        // person,post.getPlace() gives correct place name of the person
-                        if (post != null && post.getMessage() != null)
-                        {
-
-                            Connection<Comment> comments = fbClient.fetchConnection(post.getId() + "/comments",
-                                    Comment.class, Parameter.with("limit", 50000));
-
-                            for(Comment comment : comments.getData()){
-
-                                if(comment.getMessage().contains("Kaart") || comment.getMessage().contains("bankpas") ||  comment.getMessage().contains("internet bankieren") ||  comment.getMessage().contains("mobiel bankieren")
-                               || comment.getMessage().contains("pinpas")  ||  comment.getMessage().contains("sparen") || comment.getMessage().contains("spaarrekening") || comment.getMessage().contains("Mobiel")) {
-                                    String mess = comment.getMessage().replaceAll("\n", " ").replaceAll("\r", " ");
-                                    System.out.println(mess);
-                                        feeds.add(new Feeds(comment.getCreatedTime().toString(),comment.getFrom().getName(), mess,
-                                            comment.getId(), "", post.getPlace() !=null ? post.getPlace().getLocationAsString() : null));
-                                }
-                            }
-
-
-                          //  feeds.add(new Feeds(post.getCreatedTime().toString(), post.getName(), post.getMessage(),
-                              //      post.getId(), "", post.getPlace() !=null ? post.getPlace().getLocationAsString() : null));
-                          //  System.out.println(post.getCreatedTime() + " " + page.getName() + " " + post.getMessage());
-                            noOfPost++;
-                        }
-
+        System.out.println( pageResults.getData().size() + " pages searched for" + searchText);
+        for (Page page : pageResults.getData()) {
+            Connection<Post> postFeed = fbClient.fetchConnection(page.getId() + "/feed", Post.class);
+            System.out.println("No of Posts in " + page.getName() + " is " + postFeed.getData().size() + " and url is fb.com/" + page.getId());
+            /** Search Post **/
+            for (Post post : postFeed.getData()) {
+                if (post != null && post.getMessage() != null) {
+                    Connection<Comment> comments = fbClient.fetchConnection(post.getId() + "/comments",
+                            Comment.class, Parameter.with("limit", 50000));
+                    /** Search Comments **/
+                    for (Comment comment : comments.getData()) {
+                        String mess = comment.getMessage().replaceAll("\n", " ").replaceAll("\r", " ");
+                        // if(comment.getMessage().contains("Kaart") || comment.getMessage().contains("bankpas") ||  comment.getMessage().contains("internet") ||  comment.getMessage().contains("mobiel bankieren")
+                        //       || comment.getMessage().contains("pinpas")  ||  comment.getMessage().contains("sparen") || comment.getMessage().contains("spaarrekening") || comment.getMessage().contains("mobiel")) {
+                        feeds.add(new Feeds(comment.getCreatedTime().toString(), comment.getFrom().getName(), mess,
+                                comment.getId(), "", post.getPlace() != null ? post.getPlace().getLocationAsString() : null));
+                        // }
                     }
-                    noOfPosts++;
-                //}
-
-                noOfPagesSearched++;
-
-                if (noOfPagesSearched > 2) {
-                    break;
                 }
             }
         }
-        System.out.println(noOfPagesSearched + " pages searched");
         return feeds;
 
     }
