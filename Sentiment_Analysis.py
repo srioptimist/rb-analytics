d02a55e8518db02fd9b8b1450e167f604b092cdb
Vishwesh <visudevaram@gmail.com>
2016-11-27 15:02:10 +0100

    Final version
    - Display times 
    - language is set to 'en' for entity API
    - File name is taken as argument

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -35,7 +35,53 @@ for line in reader:
    request = service.documents().annotateText(
           body={
               'document':{
+import csv
+import argparse
+from googleapiclient import discovery
+import time
+from time import gmtime, strftime
+from datetime import datetime
+import pprint
+import sys
+from apiclient.discovery import build
+
+api_key = 'AIzaSyChR_RVUHz6eBVUf9BgjKxJVCyHeiCNDXk'
+
+
+s1 = strftime("%H:%M:%S", gmtime())
+print('Starting time: ', s1)
+
+# Take file name as argument 
+fl = sys.argv[1]
+
+# Create input and output file names 
+ip_file = fl + '.txt'
+op_sent_file = fl + '_sentiment_output.txt'
+op_ent_file = fl + '_entity_output.txt' 
+
+
+i = 0
+k = [None] * 5
+l = [' ',' ',' ',' ',' ']
+g = open(op_sent_file, 'w',newline='',encoding='utf-8')
+h = open(op_ent_file, 'w',newline='',encoding='utf-8')
+f = open(ip_file, 'r')
+reader = csv.reader(f, delimiter='\t')
+writer = csv.writer(g, delimiter='\t')
+writer_ent = csv.writer(h,delimiter ='\t')
+for line in reader:
+   i = i+1
+   service = build('translate', 'v2', developerKey=api_key)
+   #request = service.translations().list(source='nl',target='en', q=line[3])
+   request = service.translations().list(target='en', q=line[3])
+   response = request.execute()
+   translated_text = response['translations'][0]['translatedText']
+   service = build('language','v1beta1',developerKey=api_key)
+   request = service.documents().annotateText(
+          body={
+              'document':{
                   'type':'PLAIN_TEXT',
+				  'language':'en',
                   'content': translated_text,
                },
               'features':{
@@ -45,19 +91,6 @@ for line in reader:
               }
             )
    response = request.execute()
-
-   ent_list = response['entities']
-
-   for ent in ent_list:
-       entity_name = ent['name']
-       entity_type = ent['type'] 
-       l[1] = line[0]
-       l[2] = line[1]
-       l[3] = entity_type
-       l[4] = entity_name
-       writer_ent.writerow(l)
-
-
    x_list = response['sentences']
 
    y = 0
@@ -78,6 +111,20 @@ for line in reader:
    k[3] = translated_text
    k[4] = line[1]
    writer.writerow(k)
+
+
+   ent_list = response['entities']
+
+   for ent in ent_list:
+       entity_name = ent['name']
+       entity_type = ent['type'] 
+       l[0] = line[0]
+       l[1] = line[1]
+       l[2] = entity_type
+       l[3] = entity_name
+       l[4] = round(y,2)
+       writer_ent.writerow(l)
+
    
 s2 = strftime("%H:%M:%S", gmtime())
 print('Finishing time: ', s2)
