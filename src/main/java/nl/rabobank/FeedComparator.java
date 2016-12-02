7c5df045252b4d6812492515131ddffc4f30288d
Harikrishnan <S.Harikrishnan@rn.rabobank.nl>
2016-12-02 13:28:16 +0100

    [MINOR] Adjusted the comparator logic

--- src/main/java/nl/rabobank/FeedComparator.java
+++ src/main/java/nl/rabobank/FeedComparator.java
@@ -1,6 +1,10 @@
 package nl.rabobank;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Locale;
 
 /**
  * Created by JayaramanJ on 25-11-2016.
@@ -9,12 +13,19 @@ public class FeedComparator implements Comparator<Feeds> {
 
      public int compare(Feeds f1, Feeds f2) {
 
-        int result = f2.getSource().compareTo(f2.getSource());
+        int result = f2.getSource().compareTo(f1.getSource());
         if (result != 0)
         {
             return result;
         }
-        result = f2.getFeedDate().compareTo(f1.getFeedDate());
+        final DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH);
+        
+        try {
+            result = format.parse(f2.getFeedDate()).compareTo(format.parse(f1.getFeedDate()));
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         if (result != 0)
         {
             return result;
