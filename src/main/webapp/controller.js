38f537fec5956d80894d175a6bb002d93f12928d
jayaramj <Jagannathan.Jayaraman@rabobank.nl>
2016-12-08 16:35:22 +0100

    added spinners

--- src/main/webapp/controller.js
+++ src/main/webapp/controller.js
@@ -6,14 +6,16 @@ rbAnalyticsController.controller("rbAnalyticsCtrl",['$scope','$http',function($s
 	$scope.flag = false;
 	
 	$scope.searchFeeds = function(searchText){
-		
+		$scope.loading = true;
 		$http.get("rest/rbAnalytics/"+searchText+"")
 	    .success(
 	    			function(response) {
 	    				$scope.feeds = response;
 	    				$scope.flag = true;
 	    			}
-	    );
+	    ).finally(function() {
+	        $scope.loading = false;
+	    });
 	};
 	
   $scope.clearFeeds = function(){
